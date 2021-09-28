fac7fd7196fa0aacd6fccb3678689e85914f6b43
Robson Henrique Pereira Nascimento <rhnascimento@latam.stefanini.com>
2021-09-28 15:17:39 -0300

    Primeiro Commit

---
+++ API/Data/Migrations/20210928163510_InitialCreate1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "User",
+                table: "Users",
+                newName: "UserName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "User");
+        }
+    }
+}
